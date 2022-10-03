0b3ea278cf56fe39411e7098cfcd8e5763ee4ac0
poloz <polozfr@laposte.net>
2022-10-03 14:30:14 +0200

    Fix typo when calling wazuh_filebeat_module variable

--- manifests/filebeat_oss.pp
+++ manifests/filebeat_oss.pp
@@ -53,9 +53,15 @@ class wazuh::filebeat_oss (
     require => Package['filebeat'],
   }
 
-  archive { "/tmp/${$wazuh_filebeat_module}":
+  $wazuh_filebeat_module_dir = "/opt/filebeat_wazuh_modules/${basename($wazuh_filebeat_module, '.tar.gz')}"
+
+  file { ['/opt/filebeat_wazuh_modules', $wazuh_filebeat_module_dir]:
+    ensure => directory,
+    mode   => '0755',
+  }
+  -> archive { "/tmp/${wazuh_filebeat_module}":
     ensure       => present,
-    source       => "https://packages.wazuh.com/4.x/filebeat/${$wazuh_filebeat_module}",
+    source       => "https://packages.wazuh.com/4.x/filebeat/${wazuh_filebeat_module}",
     extract      => true,
     extract_path => '/usr/share/filebeat/module',
     creates      => '/usr/share/filebeat/module/wazuh',
